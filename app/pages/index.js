00164d94436bae532a529d4c5e05ec420cc77ba5
IDontKnowAnythng <zaved9854@gmail.com>
2024-06-06 21:52:23 -0700

    change three dots into four

--- app/pages/index.js
+++ app/pages/index.js
@@ -17,7 +17,7 @@ const Home = () => {
                         <div className={`${styles.todoCheckbox} ${styles.checked}`} />
                         <div className={styles.inputContainer}>
                             <form onSubmit={addStaticTodo}>
-                                <input value = {input} onChange={handleChange} id={styles.inputField} type="text" placeholder='Create a new todo...' />
+                                <input value = {input} onChange={handleChange} id={styles.inputField} type="text" placeholder='Create a new todo....' />
                             </form>
                         </div>
                         <div className={styles.iconContainer}>
